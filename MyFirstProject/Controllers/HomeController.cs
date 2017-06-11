c58faab752f3b3cf4b1cadd4a879fc82c4bd86f7
lucas <lukas_boo@hotmail.com>
2017-06-11 12:10:06 -0300

    first commit

---
+++ MyFirstProject/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Action Methods
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            //ViewBag.Message = "Your application description page.";
+            //return "This is the Home Controller - About Method";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            ViewBag.ijk = "Hello this is the use of view bag";
+            return View();
+        }
+    }
+}
